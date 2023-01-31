6a8065776c0a614c5e1d0b3101aa9ae5ab14d385
Cianuro-83 <buzzeosimone@yahoo.com>
2023-01-31 14:39:36 +0100

    FEAT: richiesta dati all'ìutente

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 console.log("TEST");
+// creo variabile con il prezzo del biglietto al km
+const prezzoAlKm = 0.21;
+// chiedo nome all'utente
+let name = prompt("Inserisci il tuo Nome");
+console.log(name);
+// chiedo cognome all'utente
+let surname = prompt("Inserisci il tuo Cognome");
+console.log(surname);
+// chiedo l'anno di nascita all'utente
+let birdht = parseInt(prompt("Inserisci il tuo anno di nascita"));
+console.log(birdht);
+// chiedo all'utente quanti km vuole percorrere
+let travel = parseFloat(prompt("Quanti km sarà lungo il tuo viaggio?"));
+console.log(travel);
