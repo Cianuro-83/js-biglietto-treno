18976fa30704a4f2eaf0c2c37c6eefcdd0e20366
Cianuro-83 <buzzeosimone@yahoo.com>
2023-01-31 15:42:29 +0100

    FEAT: Cal. sconticonti NON ancora in base all'età

--- js/script.js
+++ js/script.js
@@ -13,3 +13,20 @@ console.log(birdht);
 // chiedo all'utente quanti km vuole percorrere
 let travel = parseFloat(prompt("Quanti km sarà lungo il tuo viaggio?"));
 console.log(travel);
+// calcolo il costo del viaggio a prezzo pieno
+let travelFull = parseFloat(prezzoAlKm * travel);
+console.log("Il costo del biglietto è di €", travelFull.toFixed(2));
+// calcolo il costo del viaggio per i minorenni (-20%)
+let discountYoung = parseFloat([(travelFull / 100) * 20] - travelFull);
+let travelYoung = parseFloat(discountYoung * -1);
+console.log(
+  "Con lo sconto del 20% relativo alla tua età, il costo del tuo biglietto è di €",
+  travelYoung.toFixed(2)
+);
+// Calcolo il costo del viaggio per gli over 65 (-40%)
+let discountOlder = parseFloat([(travelFull / 100) * 40] - travelFull);
+let travelOlder = parseFloat(discountOlder * -1);
+console.log(
+  "Con lo sconto del 40% relativo alla tua età, il costo del tuo biglietto è di €",
+  travelOlder.toFixed(2)
+);
